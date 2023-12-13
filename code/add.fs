22fbb77b66e50f2766fec62324a3a4680b066890
yuuki1293 <yuuki1293@outlook.jp>
2023-12-13 16:20:31 +0000

    add.fsを更新

--- code/add.fs
+++ code/add.fs
@@ -5,3 +5,8 @@ module Add =
     let add_2 (a: int, b: int): int = a + b
     let add_3 ((a, b): int * int): int = a + b
     let add_4 ((a, b): struct (int * int)) = a + b
+    
+    add_1 1 2    |> printfn "%d"
+    add_2 (1, 2) |> printfn "%d"
+    add_3 (1, 2) |> printfn "%d"
+    add_4 (1, 2) |> printfn "%d"
