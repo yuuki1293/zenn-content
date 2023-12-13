106e0b01687e68cb35e12a42cc213f4f15cfd53b
yuuki1293 <yuuki1293@outlook.jp>
2023-12-13 16:51:02 +0000

    Add.fsにコメントを追加

--- code/Add.fs
+++ code/Add.fs
@@ -1,9 +1,16 @@
 namespace Fs
 
 module Add =
+    // int -> int -> int
     let add_1 (a: int) (b: int): int = a + b
+
+    // int * int -> int
     let add_2 (a: int, b: int): int = a + b
+
+    // int * int -> int
     let add_3 ((a, b): int * int): int = a + b
+
+    // struct (int * int) -> int
     let add_4 ((a, b): struct (int * int)) = a + b
     
     add_1 1 2    |> printfn "%d"
@@ -12,5 +19,8 @@ module Add =
     add_4 (1, 2) |> printfn "%d"
 
 type AddClass =
+    // int * int -> int
     static member add (a: int, b: int): int = a + b
+
+    // int * int -> int
     static member add ((a, b): int * int): int = a + b
